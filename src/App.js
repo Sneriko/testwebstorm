8465f5d4f2110b9ebeb64cb0ed799377e6b5248b
Sneriko <erik.lenas@gmail.com>
2019-07-24 14:50:11 +0200

    fgfgfg

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Charbox from './Charbox/Charbox'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+
+  state = {
+		inputString: ''
+  }
+
+	changeState = ( event ) => {
+		this.setState( { inputString: event.target.value } )
+	}
+
+	deleteCharHandler(charIndex) {
+  	let inputStr = this.state.inputString
+		const a = inputStr.split('');
+		a.splice(charIndex, 1);
+		inputStr = a.join('');
+		this.setState( { inputString: inputStr } )
+	}
+
+
+
+  render() {
+
+  	const charArray = this.state.inputString.split('');
+  	console.log(charArray);
+  	let charboxes = charArray.map((character, index) => {
+  		return <Charbox letter={character} click={() => this.deleteCharHandler(index)} />
+  	})
+
+  	return (
+			<div>
+				<input type={'text'} onChange={this.changeState} value={this.state.inputString}/>
+				{charboxes}
+
+      </div>
+		);
+	}
 }
 
 export default App;
